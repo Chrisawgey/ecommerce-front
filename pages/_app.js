768f0beb682732edc720d0d6e346fff06f86cb69
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-01 15:24:52 -0400

    fixing gap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  body{
+    padding:0;
+    margin: 0;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  
+  );
 }
