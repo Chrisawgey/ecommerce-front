260d39b4a6e2744e56bc68b11f1b396d2f648dcf
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-01 15:20:35 -0400

    adding styled

--- components/header.js
+++ components/header.js
@@ -1,8 +1,13 @@
 import Link from "next/link"
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    background-color: #222;
+`; 
 
 export default function Header() {
     return(
-        <header>
+        <StyledHeader>
             <Link href={'/'}>PlaceHolder</Link>
             <nav>
                 <Link href={'/'}>Home</Link>
@@ -11,6 +16,6 @@ export default function Header() {
                 <Link href={'/account'}>Account</Link>
                 <Link href={'/cart'}>Cart (0)</Link>
             </nav>
-        </header>
+        </StyledHeader>
     );
 }
