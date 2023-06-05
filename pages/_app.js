a0664eecfce2f630150218b28551dce0a537007b
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-05 13:43:55 -0400

    font change

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/
+  css2?family=Roboto:ital,wght@1,100&display=swap');
   body{
     padding:0;
     margin: 0;
+    font-family: 'Roboto', sans-serif;
   }
 `;
 
