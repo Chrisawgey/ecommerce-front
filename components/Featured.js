2c1ef6c52b0a0a66a43af44eb984e47e24658acb
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-06 16:16:32 -0400

    fixng fonts

--- components/Featured.js
+++ components/Featured.js
@@ -3,6 +3,8 @@ import Center from "./Center";
 
 const Bg = styled.div`
     background-color: #222;
+    color: #fff;
+    padding: 50px 0;
 `;
 
 const Title = styled.h1`
@@ -10,14 +12,38 @@ const Title = styled.h1`
     font-weight: normal;
 `;
 
+const Desc = styled.p`
+    color: #aaa;
+    font-size: .8rem;
+`;
+
+const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 40px;
+    img{
+        max-width: 100%
+    }
+`;
+
 export default function Featured() {
     return (
         <Bg>
             <Center>
-            <Title>Pro anywhere</Title>
-            <p>lorem ipsum doloe sit wais dhabdflbad
-            dfsfuhdsl dflsd fs, saudghsadsjfh 
-            sa dusfgldf ugudfgdalsjgldjshaldkjshakjfglgf</p>
+                <Wrapper>
+                <div>
+                <Title>Pro anywhere</Title>
+            <Desc>orem ipsum doloe sit wais dhabdflbad
+            dfsfuhdsl dflsd fs, saudghsadsjfh  sa dusfgldf ugudfgdalsjgldjshaldkjshakjfglgforem 
+            sa dusfgldf ugudfgdalsjgldjshaldkjshakjfglgorem ipsum doloe sit wais dhabdflbad
+            dfsfuhdsl dflsd fs, saudghsadsjfh sa dusfgldf ugudfgdalsjgldjshaldkjshakjfglgforem 
+            dfsfuhdsl dflsd fs, saudghsadsjfh sa dusfgldf ugudfgdalsjgldjshaldkjshakjfglg
+            dfsfuhdsl dflsd fs, saudghsadsjfh dfsfuhdsl dflsd fs, saudghsadsjfh </Desc>
+                </div>
+                <div>
+                    <img src="https://chriz-next-ecommerce.s3.amazonaws.com/1686082513730.png" alt="" />
+                </div>
+                </Wrapper>
             </Center>
          </Bg>
     );
