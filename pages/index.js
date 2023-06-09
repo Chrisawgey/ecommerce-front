a6c19abf805fa3691583c18c8636c89a4e133022
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-09 09:02:35 -0400

    beofre the bug

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,12 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
 
-export default function HomePage({product}) {
+export default function HomePage({featuredProduct,newProducts}) {
+  console.log({newProducts});
   return(
     <div>
       <Header />
-      <Featured product={product}/>
+      <Featured product={featuredProduct} />
       <NewProducts />
     </div>
   );
@@ -18,8 +19,10 @@ export default function HomePage({product}) {
 export async function getServerSideProps() {
   const featuredProductId = '6465876f3baa9938b8234401';
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
   return {
-    props: {product: JSON.parse(JSON.stringify(product))},
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct))},
   }
 }
