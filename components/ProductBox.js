0d4022a7b802b2cfcec2110b1aced4ee42256c77
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-13 16:02:03 -0400

    fixing img crop

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -1,14 +1,31 @@
 import styled from "styled-components"
 
-const Box = styled.div`
-    background-color: #fff;
-    padding: 20px;
+const ProductWrapper = styled.div`
+
 `;
 
-export default function ProductBox({_id,title,description,price}) {
+const WhiteBox = styled.div`
+    background-color: #fff;
+    padding: 20px;
+    height: 150px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    img{
+        max-width: 100%;
+        max-height: 150px;
+    }
+`;
+
+export default function ProductBox({_id,title,description,price,images}) {
     return(
-        <Box>
-            {title}
-        </Box>
+    <ProductWrapper>
+        <WhiteBox>
+        <div>
+        <img src={images[0]} alt=""/>
+        </div>
+        </WhiteBox>
+        {title}
+    </ProductWrapper>
     )
 }
