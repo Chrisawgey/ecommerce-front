93525aa36e1e6c8b5356c544d618e5ac502bc167
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-14 15:24:35 -0400

    arrivals!

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -9,10 +9,16 @@ const ProductsGrid = styled.div`
     padding-top: 30px;
 `;
 
+const Title = styled.h2`
+    font-size: 2rem;
+    margin: 30px 0 20px;
+    font-weight: 500;
+`;
+
 export default function NewProducts ({products}) {
     return(
         <Center>
-            <h2>New Arrivals</h2>
+            <Title>New Arrivals</Title>
             <ProductsGrid>
             {products?.length > 0 && products.map(product => (
                 <ProductBox {...product}/>
