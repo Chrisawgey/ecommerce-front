a2ef4699ce542a66e50707dc2bd7c764b41056b0
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-14 15:47:39 -0400

    adding cart functions

--- components/Featured.js
+++ components/Featured.js
@@ -3,6 +3,8 @@ import Center from "./Center";
 import Button from "./Button";
 import ButtonLink from "./ButtonLink";
 import CartIcon from "./icons/Cart";
+import { useContext } from "react";
+import { CartContext } from "./CartContext";
 
 const Bg = styled.div`
     background-color: #222;
@@ -40,6 +42,10 @@ const ButtonsWrapper = styled.div`
 `;
 
 export default function Featured({product}) {
+    const {setCartProducts} = useContext(CartContext);
+    function addFeaturedToCart () {
+        setCartProducts(prev => [...prev,product._id]);
+    }
     return (
         <Bg>
             <Center>
@@ -50,7 +56,7 @@ export default function Featured({product}) {
             <Desc>{product.description}</Desc>
             <ButtonsWrapper>
             <ButtonLink href={'/products/'+product._id} outline={1} blanco={1}>Read More</ButtonLink>
-                <Button blanco>
+                <Button blanco onClick={addFeaturedToCart}>
                 <CartIcon />
                 Add to cart
                 </Button>
