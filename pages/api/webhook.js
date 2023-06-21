185f86df5980bb986392e1db2642ef61c6b4213f
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-21 16:53:04 -0400

    adding to webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,4 +2,25 @@ import { mongooseConnect } from "@/lib/mongoose";
 
 export default async function handler(req,res) {
     await mongooseConnect();
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+
+    try {
+        event = string.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+    }
+
+    //Handle the event
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntentSucceeded = event.data.object;
+
+            break;
+
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+    }
 }
