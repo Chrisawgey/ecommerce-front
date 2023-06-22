5d335b6df4c06b6583f89290fe12a6434fc7bf3c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-22 15:51:05 -0400

    starting all products

---
+++ pages/products.js
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+export default function ProductsPage () {
+    return(
+        <>
+            <Header />
+            <h1>All Prod</h1>
+         </>
+
+     );
+}
