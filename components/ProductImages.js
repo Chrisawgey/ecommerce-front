7c728973aff1d57f44c45cea05333eb71258309c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-28 16:15:21 -0400

    opac

--- components/ProductImages.js
+++ components/ProductImages.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import styled from "styled-components"
 
-const Image = styled.img`
+    const Image = styled.img`
         max-width: 100%;
         max-height: 100%;
+    `;
+    const BigImage = styled.img`
+        max-width: 100%;
+        max-height: 200px;
     `;
     const ImageButtons = styled.div`
         display: flex;
@@ -12,20 +16,34 @@ const Image = styled.img`
     `;
     const ImageButton = styled.div`
         border: 2px solid #ccc;
+        ${props => props.active ?  `
+            border-color: #ccc;
+        ` : `
+            border-color: transparent;
+            opacity: .6;
+        `}
         height: 40px;
         padding: 2px;
         cursor: pointer;
         border-radius: 5px;
+    `;
+    const BigImageWrapper = styled.div`
+        text-align: center;
     `;
 
 export default function ProductImages({images}) {
     const [activeImage,setActivceImage] = useState(images?.[0]);
      return (
         <>
-            <Image src={activeImage} />
+            <BigImageWrapper>
+                <BigImage src={activeImage} />
+            </BigImageWrapper>
             <ImageButtons>
                 {images.map(image => (
-                    <ImageButton onClick={() => setActivceImage(image)}>
+                    <ImageButton 
+                    key={image}
+                    active={image===activeImage}
+                     onClick={() => setActivceImage(image)}>
                         <Image src={image} alt=""/>
                     </ImageButton>
                 ))}
