162c9edd520f49b86b2537519d3471875cdef42c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-29 16:03:51 -0400

    mobile

--- components/header.js
+++ components/header.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Center from "@/components/Center";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import BarsIcon from "./icons/Bars";
 
 const StyledHeader = styled.header`
     background-color: #222;
@@ -18,16 +19,36 @@ const Wrapper = styled.div`
     padding: 20px 0;
 `;
 const StyledNav = styled.nav`
-display: flex;
+display: block;
 gap: 15px;
+position: fixed;
+top: 50px;
+bottom: 0;
+left: 0;
+right: 0;
+padding: 20px;
+background-color: #222;
+@media screen and (min-width: 768px) {
+    display: flex;
+    position: static;
+    padding: 0;
+}
 `;
 const NavLink = styled(Link)`
+display: block;
 color: #aaa;
 text-decoration: none;
 `;
-
+const NavButton = styled.button`
+    background-color: transparent;
+    width: 30px;
+    height: 30px;
+    border: 0;
+    color: white;
+    cursor: pointer;
+`;
 export default function Header() {
-    const {cartProducts} = useContext(CartContext)
+    const {cartProducts} = useContext(CartContext);
     return (
         <StyledHeader>
             <Center>
@@ -42,6 +63,10 @@ export default function Header() {
                     <NavLink href={'/account'}>Account</NavLink>
                     <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
                 </StyledNav>
+                <NavButton>
+                <BarsIcon />
+
+                </NavButton>
                 </Wrapper>
             </Center>
         </StyledHeader>
