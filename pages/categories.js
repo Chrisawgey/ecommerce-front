d1ff7b1ccf6deb8cf7dd6c65095f26fb928a657c
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-30 09:14:00 -0400

    starting categories

---
+++ pages/categories.js
@@ -0,0 +1,14 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+
+export default function CategoriesPage() {
+    return(
+        <>
+            <Header />
+            <Center>
+             <Title>All categories</Title>
+            </Center>
+        </>
+    );
+}
