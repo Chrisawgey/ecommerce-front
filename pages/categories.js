0e5109308a0d276d335156b4b1ae1a61e34846b5
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-30 15:01:13 -0400

    catIds

--- pages/categories.js
+++ pages/categories.js
@@ -18,7 +18,7 @@ const CategoryGrid = styled.div`
 
 const CategoryTitle = styled.div`
     display: flex;
-    margin-top: 40px;
+    margin-top: 10px;
     margin-bottom: 0;
     align-items: center;
     gap: 15px;
--- pages/category/[id].js
+++ pages/category/[id].js
@@ -1,14 +1,17 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
+import ProductsGrid from "@/components/ProductsGrid";
 import Title from "@/components/Title";
 import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
 
-export default function CategoryPage({category}) {
+export default function CategoryPage({category,products}) {
     return(
         <>
             <Header />
             <Center>
                 <Title>{category.name}</Title>
+                <ProductsGrid products={products}/>
             </Center>
         </>
     );
@@ -16,9 +19,13 @@ export default function CategoryPage({category}) {
 
 export async function getServerSideProps(context) {
     const category = await Category.findById(context.query.id);
+    const subCategories = await Category.find({parent:category._id});
+    const catIds = [category._id, ...subCategories.map(c => c._id)];
+    const products = await Product.find({category:catIds})
     return{
         props:{
             category: JSON.parse(JSON.stringify(category)),
+            products: JSON.parse(JSON.stringify(products)),
         }
     }
 }
