a821aa1b9bc835ba74a452939d2d9ef7b422cbe9
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-06-30 09:46:06 -0400

    fixing categories and calssifying names

--- pages/categories.js
+++ pages/categories.js
@@ -1,14 +1,44 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import Title from "@/components/Title";
+import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
 
-export default function CategoriesPage() {
+export default function CategoriesPage({mainCategories,categoriesProducts}) {
     return(
         <>
             <Header />
             <Center>
-             <Title>All categories</Title>
+             {mainCategories.map(cat => (
+                <div>
+                    <h2>{cat.name}</h2>
+                    <div>
+                        {categoriesProducts[cat._id].map(p => (
+                            <div>{p.title}</div>
+                        ))}
+                    </div>
+                </div>
+             ))}
             </Center>
         </>
     );
+}
+
+export async function getServerSideProps() {
+    const categories = await Category.find();
+    const mainCategories = categories.filter(c => !c.parent);
+    const categoriesProducts = {}; // catId => [products]
+    for(const mainCat of mainCategories){
+        const products = await Product.find({category: mainCat._id}, null,
+             {limit:3,sort:{'_id':-1}});
+        categoriesProducts[mainCat._id] = products;
+    }
+    return {
+        props: {
+            mainCategories: JSON.parse(
+                JSON.stringify(mainCategories)
+                ),
+                categoriesProducts: JSON.parse(JSON.stringify(categoriesProducts)),
+        },
+    };
 }
