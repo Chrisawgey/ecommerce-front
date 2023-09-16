b4da222e5fc79fea366a33796da3a61683eefc9a
Chrisawgey <124189207+Chrisawgey@users.noreply.github.com>
2023-09-15 23:16:43 -0400

    saving after auth proc

---
+++ pages/account.js
@@ -0,0 +1,14 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+
+export default function AccountPage() {
+    return(
+        <>
+            <Header/>
+            <Center>
+                <Title>Account</Title>
+            </Center>
+        </>
+    )
+}
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import clientPromise from '@/lib/mongods'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+  ],
+    adapter: MongoDBAdapter(clientPromise),
+ 
+}
+
+export default NextAuth(authOptions);
+
