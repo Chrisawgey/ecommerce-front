0b45a9d3c5d69c9c1fc5fa64115af70e4408f2bd
Chrisawgey <chrisvpopoca@gmail.com>
2024-01-31 10:53:09 -0500

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import Featured from "@/components/Featured";
+import {Product} from "@/models/Product";
+import {mongooseConnect} from "@/lib/mongoose";
 import NewProducts from "@/components/NewProducts";
-import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/Product";
-import { Setting } from "@/models/Setting";
-import { authOptions } from "./api/auth/[...nextauth]";
-import { WishedProduct } from "@/models/WishedProduct";
-import { getServerSession } from "next-auth";
-
+import {WishedProduct} from "@/models/WishedProduct";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import {Setting} from "@/models/Setting";
 
 export default function HomePage({featuredProduct,newProducts,wishedNewProducts}) {
   return (
@@ -20,23 +19,23 @@ export default function HomePage({featuredProduct,newProducts,wishedNewProducts}
 }
 
 export async function getServerSideProps(ctx) {
+  await mongooseConnect();
   const featuredProductSetting = await Setting.findOne({name:'featuredProductId'});
   const featuredProductId = featuredProductSetting.value;
-  await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
   const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
   const session = await getServerSession(ctx.req, ctx.res, authOptions);
   const wishedNewProducts = session?.user
     ? await WishedProduct.find({
-      userEmail:session.user.email,
-      product: newProducts.map(p => p._id.toString()),
-    })
+        userEmail:session.user.email,
+        product: newProducts.map(p => p._id.toString()),
+      })
     : [];
-    return {
-      props: {
-        featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
-        newProducts: JSON.parse(JSON.stringify(newProducts)),
-        wishedNewProducts: wishedNewProducts.map(i => i.product.toString()),
-      },
-    };
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+      wishedNewProducts: wishedNewProducts.map(i => i.product.toString()),
+    },
+  };
 }
